a3be5efcc009ced0defd1b92f768d5afeb7cc492
Carla <carvigil21@gmail.com>
2016-09-30 00:33:33 -0300

    Agregado de atributos, get, set y constructores

--- src/entities/jugador.java
+++ src/entities/jugador.java
@@ -2,6 +2,31 @@ package entities;
 
 public class jugador {
 	
-	String nombreJugador;
+	String nombre;
+	String apellido;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}	
+	
+
+// Constructores
+	
+public jugador(){
+
+}
+public jugador(String nom, String ape){
+	setNombre(nom);
+	setApellido(ape);
+}
+
 }
